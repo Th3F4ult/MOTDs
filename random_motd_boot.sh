175af893bb96d9aafa526892a37f45d747132f33
Th3F4ult <th3f4ult@gmail.com>
2025-09-21 18:59:39 +0200

    Added more MOTDs. Separated them into categories.

--- random_motd_boot.sh
+++ random_motd_boot.sh
@@ -2,15 +2,18 @@
 
 # All our MOTDs, Bash doesn't need commas.
 MOTDS=(
+
+# Computer stuff
 "Now with IPv6 support"
 "I'm in"
 "Approved by some"
 "Change this message by editing /etc/mood"
-"Support free software" "To GNU or not to GNU, there lies the question"
+"Support free software"
+"To GNU or not to GNU, there lies the question"
 "We <3 Tux"
 "Running since 1970"
 "Password too short, please change"
-"Doas>Sudo"
+"Doas > Sudo"
 "Now with 40% more 1s and 40% less 0s"
 "Production machine please don't mess up"
 "Kernel changelog: Added 3 easter eggs to the kernel panic screen"
@@ -19,30 +22,64 @@ MOTDS=(
 "[ ] Please complete the captcha"
 "> Hack mainframe.py.sh.php.css"
 "I know what I'm doing, don't worry"
-"Stop the experimentation for free cake"
 "It's been [0] days since accidentally rm -rf /-ing the whole system"
 "I swear I'm not a hacker"
 "Do no hack"
-"I'ts ok, I love you too"
 "$ man woman"
-"Down the electric avenue"
 "Unregistered OpenSSH_10.0p2"
 "It works on my machine"
-"For the good of all of us... Except the ones who are dead... But there's no sense crying over every mistake"
 "Looks good to me!"
-"We do what we must because we can"
+
+# Possibly controversial - You might want to remove these
+"I'm not a furry but Xenia's got something ngl"
+"Who's a good girl? Yes, you! You are!"
+"Femboy radar enabled... BEEP BEEP BEEP BEEP"
+"Damn baby you've got that sigma gyatt"
+
+# General non-computer stuff
+"It's ok, I love you too"
 "Mommy is proud <3"
+"[x] Fight [ ] Bag [ ] Run"
+"You're breaking my heart </3"
+"This is not a place of honor. Nothing of value is here. What is here was repulsive to us."
+"There was an error loading this motd file: I'm unMOTDtivated... haha, get it?"
+"How was your day, dear?"
+"#MadeWithNoAI"
+
+# Also try
 "Also try FreeBSD"
 "Also try Plan9!"
 "Also try Solaris!"
 "Also try MacOS!"
-"[x] Fight [ ] Bag [ ] Run"
+
+# Pop culture references
+"The Matrix has you, Neo."
+"This Linuxing session is going to be Legend... Wait for it... -dary!"
+"I see dead people"
+"I must scream but I've got no mouth"
+"I survived the Among Us brainrot and I got was this lousy line on my /etc/motd"
 "Dear <<Subject Name Here>>,"
 "You just had to follow the damn train, CJ!"
-"You're breaking my heart </3"
-"I must scream but I've got no mouth"
-"This is not a place of honor. Nothing of value is here. What is here was repulsive to us."
+"For the good of all of us... Except the ones who are dead... But there's no sense crying over every mistake"
+"Stop the experimentation for free cake"
+"We do what we must because we can"
+
+# Music references
+"21st night of September"
+"We didn't start the fire"
+"'Cause I'm a Freeeeee Bird yeaaaaah"
+"Down the electric avenue"
+
+# The political stuff
+"B.L.M!"
+"Support Trans Rights!"
 "Fuck the fascists!"
+"Love Thy Neighbor, even if thy neighbor is gay"
+"Open borders for all"
+"No one is illegal"
+"Love eachother"
+"Fight for feminism"
+
 )
 
 array_size=${#MOTDS[@]} # Get the size of the array
